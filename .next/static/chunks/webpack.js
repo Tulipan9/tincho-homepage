8ef339abd2cfb21a2f98fcb45429ae3cc467d4e7
Tincho <martin.rios.javier@gmail.com>
2022-08-06 17:43:31 -0300

    add thems

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e50d168e743d8373"; }
+/******/ 		__webpack_require__.h = function() { return "7a92e840395b2675"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
