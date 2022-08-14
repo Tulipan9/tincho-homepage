10aa3633687cda04466f668542bf5160e23a2a7a
Tincho <martin.rios.javier@gmail.com>
2022-08-14 01:30:29 -0300

    add bio section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "becb61dae1a0399d"; }
+/******/ 		__webpack_require__.h = function() { return "069554b3b6cc6d30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
